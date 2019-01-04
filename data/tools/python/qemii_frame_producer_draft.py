55eb945e23fccabc666f10e1d2ba00be32e523bb
Sophie Kirkham <sophie.kirkham@stfc.ac.uk>
2019-01-04 12:22:42 +0000

    Drop list, cmd line args, tx interval, needs commenting

--- data/tools/python/qemii_frame_producer_draft.py
+++ data/tools/python/qemii_frame_producer_draft.py
@@ -5,6 +5,7 @@ import os
 from dpkt.compat import compat_ord
 import logging
 import time
+import argparse
 
 QEM_SOF = 128 << 24
 QEM_EOF = (64 << 24) + 7
@@ -18,12 +19,11 @@ class QEMFrame():
 
     def __init__(self, frame_num):
 
-        
         self.frame_number = frame_num
         self.packets = []   
     
     def get_packets(self):
-        return self.packets[]
+        return self.packets
     
     def append_packet(self, packet):
         self.packets.append(packet)
@@ -31,22 +31,89 @@ class QEMFrame():
     def get_num_packets(self):
         return len(self.packets)
 
+
+class QEMFrameProducerDefaults():
+
+
+    def __init__(self):
+
+        self.ip_address = 'localhost'
+        self.port = '51501'
+        self.num_frames = 0
+        self.tx_interval = 0
+        self.drop_list = None
+        self.log_level = logging.INFO
+
+        self.pcap_file = "/scratch/qem/data.pcap"
+
 class QEMFrameProducer():
 
 
     def __init__(self):
 
         self.frames = []
-        self.ip_address = 'localhost'
-        self.port = 51501
-        self.pcap_file = "/scratch/qem/data.pcap"
-        self.tx_interval = 0
-        self.log_level = 'info'
+     
+        self.defaults = QEMFrameProducerDefaults()
+
+        parser = argparse.ArgumentParser(description="QEM-II Frame Producer")
+
+        parser.add_argument(
+            'pcap_file', type=argparse.FileType('rb'),
+            default= self.defaults.pcap_file, 
+            help="Packet capture file to load."
+        )
+        parser.add_argument(
+            '--address', '-a', type=str, dest='ip_address',
+            default=self.defaults.ip_address,
+            help='IP address to send the UDP packets to'
+        )
+        parser.add_argument(
+            '--port', '-p', type=int, dest='port',
+            default=self.defaults.port,
+            help='Port number to send the UDP packets to'
+        )
+        parser.add_argument(
+            '--frames', '-n', type=int, dest='num_frames',
+            default=self.defaults.num_frames, metavar='FRAMES',
+            help='Number of frames to transmit (0 = send all frames found in packet capture file'
+        )
+        parser.add_argument(
+            '--interval', '-i', type=float, dest='tx_interval',
+            default=self.defaults.tx_interval, metavar='INTERVAL',
+            help='Interval in seconds between transmission of frames'
+        )
+        """
+        parser.add_argument(
+            '--pkt_gap', type=int, dest='pkt_gap', metavar='PACKETS',
+            help='Insert brief pause between every N packets'
+        )
+        parser.add_argument(
+            '--drop_frac', type=float, dest='drop_frac',
+            min=0.0, max=1.0,
+            default=self.defaults.drop_frac, metavar='FRACTION',
+            help='Fraction of packets to drop'
+        )
+        """
+        parser.add_argument(
+            '--drop_list', type=int, nargs='+', dest='drop_list',
+            default=self.defaults.drop_list,
+            help='Packet number(s) to drop from each frame',
+        )
+        parser.add_argument(
+            '--logging', type=str, dest='log_level',
+            default=self.defaults.log_level,
+            help='Set logging output level'
+        )
+
+        self.args = parser.parse_args()
 
         logging.basicConfig(
-            level=self.log_level, format='%(levelname)1.1s %(message)s',
+            level=self.defaults.log_level, format='%(levelname)1.1s %(message)s',
             datefmt='%y%m%d %H:%M:%S'
         )
+
+        #self.pcap_file = self.args.pcap_file
+        self.pcap = dpkt.pcap.Reader(self.args.pcap_file)
 
     def run(self):
 
@@ -55,15 +122,13 @@ class QEMFrameProducer():
 
     def load_pcap(self):
 
-        f = open(self.pcap_file)
+        #f = open(self.pcap_file)
 
         logging.info(
             "Extracting QEM frame packets from PCAP file %s",
-            self.pcap_file
+            self.args.pcap_file
         )
 
-        # Initialise the packet capture file reader
-        pcap = dpkt.pcap.Reader(f)
         total_packets = 0
         total_bytes = 0
         frame_number = -1
@@ -71,7 +136,7 @@ class QEMFrameProducer():
         # Initialise current frame
         current_frame = None
 
-        for timestamp, buffer in pcap:
+        for timestamp, buffer in self.pcap:
             
             ethernet_layer = dpkt.ethernet.Ethernet(buffer)
             ip_layer = ethernet_layer.data
@@ -87,10 +152,10 @@ class QEMFrameProducer():
             udp_packet = header + stripped_udp_packet
             
 
-            if packet_number == QEMFrame.QEM_SOF
+            if packet_number == QEMFrame.QEM_SOF:
 
                 if current_frame is not None: 
-                    if current_frame.get_num_packets() != QEM_SUB_FRAMES:
+                    if current_frame.get_num_packets() != QEMFrame.QEM_SUB_FRAMES:
                         #we didn't get a correct number of packets in the last frame before receiving a new SOF.
 
                         logging.debug(
@@ -112,6 +177,8 @@ class QEMFrameProducer():
             total_packets += 1
             total_bytes += data_length
 
+        print len(self.frames)
+
     def send_packets(self):
         # Create the UDP socket
         udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -122,25 +189,58 @@ class QEMFrameProducer():
 
         #get the length of frames
 
-        frames_to_send = len(self.frames)
+        if self.args.num_frames == 0:
+            frames_to_send = len(self.frames)
+        else:
+            frames_to_send = self.args.num_frames
+
+        total_bytes_sent = 0
+        total_frames_sent = 0
+        total_packets_sent = 0
+        total_packets_dropped = 0
+
+        logging.info(
+            "Sending %d frames to destination %s:%d", frames_to_send, self.args.ip_address, self.args.port
+        )
 
         for frame in range(frames_to_send):
 
             current_frame = self.frames[frame]
-            
             start_time = time.time()
+            frame_pkts_dropped = 0
 
-            for packet in current_frame.packet:
-
+            for packet_id, packet in enumerate(current_frame.packets):
+                
+                if self.args.drop_list is not None: 
+                    if packet_id in self.args.drop_list:
+                        frame_pkts_dropped += 1
+                        continue
 
                 # Send the packet over the UDP socket
                 try:
-                    udp_socket.sendto(packet, (self.ip_address, self.port))
-                    #frame_packets_sent += 1
+                    total_bytes_sent += udp_socket.sendto(packet, (self.args.ip_address, self.args.port))
+                    total_packets_sent += 1
+                    #PACKET GAP GOES HERE
                 
                 except socket.error as exc:
                     logging.error("Got error sending frame packet: %s", exc)
                     break
+
+            total_frames_sent += 1
+            total_packets_dropped += frame_pkts_dropped
+
+            #WAIT TIME
+            end_time = time.time()
+            wait_time = (start_time + self.args.tx_interval) - end_time
+            if wait_time > 0:
+                time.sleep(wait_time)
+
+
+        udp_socket.close()
+        logging.info(
+            "sent %d bytes, %d frames and %d packets. Dropped %d packets", 
+            total_bytes_sent, total_frames_sent, total_packets_sent, total_packets_dropped
+        )
 
 if __name__ == "__main__":
     QEMFrameProducer().run()
