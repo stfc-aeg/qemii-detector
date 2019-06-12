de8eb95cb51caa735bc1a23053e2896635239cd1
Adam Neaves <adam.neaves@stfc.ac.uk>
2019-06-12 13:45:41 +0100

    Created vector file adapter to load and manipulate vector file.

---
+++ control/QEM-Client/QemClient/VectorFileAdapter.py
@@ -0,0 +1,268 @@
+"""QEM Vector File Adapter for the QEM Detector System.
+
+Opens, Updates, and uploads vector files to the FEM.
+
+Allows a user to change DAC settings, and plot the vector file much like a logic analyser would.
+
+Adam Neaves, Application Engineering Group, STFC. 2019
+"""
+
+import logging
+import os.path
+from functools import partial
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+from odin.adapters.adapter import ApiAdapter, ApiAdapterRequest, ApiAdapterResponse, request_types, response_types
+from odin.adapters.parameter_tree import ParameterTree, ParameterTreeError
+from odin.util import decode_request_body, convert_unicode_to_string
+from tornado.ioloop import IOLoop
+
+DACDIN = 43
+BIAS_DEPTH = 6
+
+# bias names, in the order that they appear in the vector file.
+# DEFAULTS IN COMMENTS
+BIAS_NAMES = ["iBiasCol",        # 001100 - 0x0C - 12
+              "iBiasSF0",        # 000101 - 0x05 - 05
+              "vBiasPGA",        # 000000 - 0x00 - 00
+              "iBiasPGA",        # 001100 - 0x0C - 12
+              "iBiasSF1",        # 001010 - 0x0A - 10
+              "iBiasOutSF",      # 011001 - 0x19 - 25
+              "iBiasLoad",       # 010100 - 0x14 - 20
+              "iBiasADCbuffer",  # 001100 - 0x0C - 12
+              "iBiasCalC",       # 001100 - 0x0C - 12
+              "iBiasRef",        # 001010 - 0x0A - 10
+              "iCbiasP",         # 011010 - 0x1A - 26
+              "vBiasCasc",       # 100000 - 0x20 - 32
+              "iFbiasN",         # 011000 - 0x18 - 24
+              "iBiasCalF",       # 010010 - 0x12 - 18
+              "iBiasADC1",       # 010100 - 0x14 - 20
+              "iBiasADC2",       # 010100 - 0x14 - 20
+              "iBiasAmpLVDS",    # 010000 - 0x10 - 16
+              "iBiasLVDS",       # 101101 - 0x2D - 45
+              "iBiasPLL"]        # 010100 - 0x14 - 20
+
+
+class VectorFileAdapter(ApiAdapter):
+
+    def __init__(self, **kwargs):
+        """Initialize the Vector File Adapter object
+        """
+
+        super(VectorFileAdapter, self).__init__(**kwargs)
+
+        if self.options.get('file_name', False):
+            self.vector_file = VectorFile(self.options['file_name'])
+        else:
+            logging.warning("No Vector File selected.")
+
+    @response_types('application/json', default='application/json')
+    def get(self, path, request):
+
+        try:
+            response = self.vector_file.param_tree.get(path)
+            status_code = 200
+        except ParameterTreeError as e:
+            response = {"error": str(e)}
+            status_code = 400
+
+        content_type = 'application/json'
+        logging.debug("BIAS DICT: %s", self.vector_file.bias)
+        return ApiAdapterResponse(response, content_type=content_type,
+                                  status_code=status_code)
+
+    @request_types('application/json')
+    @response_types('application/json', default='application/json')
+    def put(self, path, request):
+        try:
+            data = convert_unicode_to_string(decode_request_body(request))
+            self.vector_file.param_tree.set(path, data)
+            response = self.vector_file.param_tree.get(path)
+            status_code = 200
+        except ParameterTreeError as e:
+            response = {'error': str(e)}
+            status_code = 400
+        except (TypeError, ValueError) as e:
+            response = {'error': 'Failed to decode PUT request body: {}'.format(str(e))}
+            status_code = 400
+
+        content_type = 'application/json'
+        return ApiAdapterResponse(response, content_type=content_type,
+                                  status_code=status_code)
+
+
+class VectorFile():
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.vector_loop_position = 0
+        self.vector_length = 0
+        self.vector_names = []
+        self.vector_data = []
+
+        self.dac_clock_refs = []
+        self.dac_data_vector = []
+
+        self.bias = {}
+
+        self.clock_step = 0
+        
+        self.get_vector_information()
+        
+        self.bias_tree = ParameterTree(
+            # dict comprehension, like a one-line for loop
+            # basically, for each bias, create a tuple of partial functions
+            # that get/set values from the dictionary
+            {bias_name: (partial(self.get_bias_val, bias_name),
+                         partial(self.set_bias_val, bias_name),
+                         # metadata ensures the bias val can't go over its 6 bit max
+                         {"min": 0, "max": (2**BIAS_DEPTH) - 1})
+             for bias_name in self.bias.keys()})
+    
+        self.param_tree = ParameterTree({
+            "bias": self.bias_tree,
+            "file_name": (lambda: self.file_name, self.set_file_name),
+            "length": (lambda: self.vector_length, None),
+            "loop_pos": (lambda: self.vector_loop_position, None),
+            "save": (None, self.write_vector_file),
+            "reset": (None, self.reset_vector_file)
+        })
+
+    def get_vector_information(self):
+        """Extract the information from the vector file, such as loop position,
+        vector length, and the vector data.
+        """
+        with open(self.file_name, 'r') as f:
+
+            self.vector_loop_position = int(f.readline())
+            self.vector_length = int(f.readline())
+            self.vector_names = f.readline().split()
+
+            self.dac_clk_in = self.vector_names.index("dacCLKin")
+            self.dac_dat_in = self.vector_names.index("dacDin")
+
+            logging.info("Loop Position:      %s", self.vector_loop_position)
+            logging.info("Vector File Length: %s", self.vector_length)
+
+            logging.info("Clock Column:       %d", self.vector_names.index("dacCLKin"))
+            logging.info("DAC Data In Column: %d", self.vector_names.index("dacDin"))
+
+            # read the remaining data from the file
+            vector_data_string = f.read()
+
+        # convert to 2d array
+        self.vector_data = vector_data_string.splitlines()
+        self.vector_data = [[int(y) for y in x] for x in self.vector_data]
+
+        logging.info("Vector Data Shape: (%d,%d)", len(self.vector_data), len(self.vector_data[0]))
+
+        self.extract_clock_references()
+
+    def extract_clock_references(self):
+        """Extract the -ve clock positions and list them.
+        Also extract the value associated with the -ve clock position and list them.
+        """
+
+        latch = 0  # stores previous value of clk_in
+        # reset the arrays for clock refs and data vectors
+        self.dac_clock_refs = []
+        self.dac_data_vector = []
+
+        # get all instances of Falling Edges for the clock ref (self.dac_clk_in) going from 1 -> 0
+        for i, row in enumerate(self.vector_data):
+            clk_in = row[self.dac_clk_in]
+            if latch != clk_in and latch == 1:
+                # state has changed from 1 to 0
+                self.dac_clock_refs.append(i)
+                self.dac_data_vector.append(row[DACDIN])
+                # logging.debug("Data on Line %d: %d", i, row[DACDIN])
+            latch = clk_in
+        self.clock_step = self.dac_clock_refs[1] - self.dac_clock_refs[0]
+        self.convert_raw_dac_data()
+
+    def convert_raw_dac_data(self):
+        """Convert the binary data from the vector files into a dictionary of actual bias values
+        and keys
+        """
+
+        for i, dac_data_name in enumerate(BIAS_NAMES):
+            # bias values are 6 bit. each value in dac_data_vector is one bit
+            data_start = i * BIAS_DEPTH
+            data_end = (i * BIAS_DEPTH) + BIAS_DEPTH
+            data = self.dac_data_vector[data_start: data_end]
+            # using join() + list comprehension
+            # converting binary list to integer
+            self.bias[dac_data_name] = int("".join(str(x) for x in data), 2)
+            logging.debug("%-16s: %2d", dac_data_name, self.bias[dac_data_name])
+
+    def convert_bias_to_raw(self):
+        """Convert the data in the bias dictionary into the binary representation required
+        in the vector file. Modify the dac_data_vector list to represent this changed data.
+        Modify the vector_data list of lists with this new data
+        """
+
+        # convert the bias values from the dict into binary values in the dac_data_vector list
+        for i, bias_name in enumerate(BIAS_NAMES):
+            bias = '{:0{depth}b}'.format(self.bias[bias_name], depth=BIAS_DEPTH)
+            logging.debug("%-16s: %s", bias_name, bias)
+            first_start = i * BIAS_DEPTH
+            second_start = (i + 19) * BIAS_DEPTH
+            self.dac_data_vector[first_start: first_start + BIAS_DEPTH] = list(bias)
+            self.dac_data_vector[second_start: second_start + BIAS_DEPTH] = list(bias)
+
+        for i, line_num in enumerate(self.dac_clock_refs):  # for each clock edge
+            # get slice of vector data, going from half the distance between clock edges above
+            for line in self.vector_data[line_num - self.clock_step: line_num + self.clock_step]:
+                line[self.dac_dat_in] = self.dac_data_vector[i]
+
+    def write_vector_file(self, file_name):
+        if file_name.lower() == "none" or file_name == "":
+            file_name = self.file_name
+
+        logging.debug("Converting Biases to Binary")
+        self.convert_bias_to_raw()
+
+        with open(file_name, 'w') as f:
+            logging.debug("FILE NAME: %s", f.name)
+            logging.debug("FILE MODE: %s", f.mode)
+            f.write("{}\n".format(self.vector_loop_position))
+            f.write("{}\n".format(self.vector_length))
+            f.write("\t".join(self.vector_names))
+            f.write('\n')
+
+            # write the actual data, line by line
+            for line in self.vector_data:
+                str_line = "".join([str(x) for x in line])
+                f.write("{}\n".format(str_line))
+
+    def set_file_name(self, name):
+        # TODO: some form of verification
+        self.file_name = name
+
+    def reset_vector_file(self, data):
+        if(os.path.isfile(self.file_name)):
+            self.get_vector_information()
+
+    def get_bias_val(self, bias_name):
+        return self.bias[bias_name]
+
+    def set_bias_val(self, bias_name, val):
+        logging.info("SETTING BIAS %s to %d", bias_name, val)
+        self.bias[bias_name] = val
+
+        # get position in list, as its in the same order in the vector_data
+        bias_pos = BIAS_NAMES.index(bias_name)
+        bin_bias = '{:0{depth}b}'.format(self.bias[bias_name], depth=BIAS_DEPTH)  # convert to binary
+        first_start = bias_pos * BIAS_DEPTH
+        second_start = (bias_pos + 19) * BIAS_DEPTH
+        self.dac_data_vector[first_start: first_start + BIAS_DEPTH] = list(bin_bias)
+        self.dac_data_vector[second_start: second_start + BIAS_DEPTH] = list(bin_bias)
+
+        # write all the biases. We only really need to write the one that changed but its easier
+        # to write them all.
+        for i, line_num in enumerate(self.dac_clock_refs):  # for each clock edge
+            # get slice of vector data, full distance of clock edge with current edge in center
+            for line in self.vector_data[line_num - self.clock_step: line_num + self.clock_step]:
+                line[self.dac_dat_in] = self.dac_data_vector[i]
